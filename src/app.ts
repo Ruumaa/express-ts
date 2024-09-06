36b4c910c2712a5d7efaeb6e8548fd56f81ae7ec
Ruumaa <titankalbuesa01@gmail.com>
2024-09-06 17:19:52 +0700

    feat: Add vercel.json configuration

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ config();
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { errorHandler } from './middleware/errorHandler';
 import { router } from './routes/routes';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT: number | string = process.env.PORT || 5000;
 
+app.use(cors());
 //middleware handle JSON request
 app.use(express.json());
 
